6156048549f136a3950d03eb0d4f645894398356
Sebastian Arguello <sebaescu@espol.edu.ec>
2025-07-27 18:28:11 -0500

    fix: Corregir workflow de CI/CD

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:8000/api/',
-  locationApiKey: 'YOUR_DEV_API_KEY_HERE' // Reemplaza con tu API key local
+  locationApiKey: 'aTIwT1FYbzZ2emJ5ZEhwSWMwR0tDMzdEMUx0SDRBNzdEV0lDcjF4NA==' // Tu API key real (NO hacer commit de esto)
 };
